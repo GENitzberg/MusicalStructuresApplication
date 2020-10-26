997f0d6f381367e899c5c71ee170ac6919eedf67
emily <emily_nitzberg@yahoo.com>
2020-10-26 14:57:16 -0700

    Set up OnClickListeners for all search options in the main activity - by song, by artist, and by album. Also, created a java class for each of the main activities which will later include the Arraylists needed for each activity

--- app/src/main/java/com/example/musicalstructuresapplication/MainActivity.java
+++ app/src/main/java/com/example/musicalstructuresapplication/MainActivity.java
@@ -2,13 +2,47 @@ package com.example.musicalstructuresapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //Setting up the content of the main activity
         setContentView(R.layout.activity_main);
+
+        //Setting up onClick listeners for all search options
+        TextView songs = (TextView) findViewById(R.id.song_names);
+        songs.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MainActivity.this, SongsActivity.class);
+                startActivity(i);
+            }
+        });
+
+        TextView artists = (TextView) findViewById(R.id.artist_names);
+        artists.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MainActivity.this, ArtistsActivity.class);
+                startActivity(i);
+            }
+        });
+
+        TextView albums = (TextView) findViewById(R.id.album_names);
+        albums.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MainActivity.this, AlbumsActivity.class);
+                startActivity(i);
+            }
+        });
+
     }
 }
---
+++ app/src/main/java/com/example/musicalstructuresapplication/SongsActivity.java
@@ -0,0 +1,4 @@
+package com.example.musicalstructuresapplication;
+
+public class SongsActivity {
+}
