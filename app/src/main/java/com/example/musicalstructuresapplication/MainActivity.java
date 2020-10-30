1ee7d33185b379d04dc3e4297bbff438480134a8
emily <emily_nitzberg@yahoo.com>
2020-10-30 13:58:38 -0700

    Removed forward and rewind methods - added OnClickListeners for the rewind and fast forward button instead to iterate through the songsArray and display the currently playing song. Updated the Currently Playing Song TextView to look nicer and more clear. Project now be completed.

--- app/src/main/java/com/example/musicalstructuresapplication/MainActivity.java
+++ app/src/main/java/com/example/musicalstructuresapplication/MainActivity.java
@@ -8,8 +8,12 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+    int index = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,7 +22,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         //Setting up onClick listeners for all search options
-        TextView songs = (TextView) findViewById(R.id.song_names);
+        final TextView songs = (TextView) findViewById(R.id.song_names);
         songs.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -44,15 +48,71 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(i);
             }
         });
-    }
 
-    /*Method to show the playing song when the forward button is pressed*/
-    public void forward(View view) {
+        final ArrayList<Song> songsArray = new ArrayList<Song>();
+        songsArray.add(new Song("Hello", "Adele", "25"));
+        songsArray.add(new Song("Water Under the Bridge", "Adele", "25"));
+        songsArray.add(new Song("I Miss You", "Adele", "25"));
+        songsArray.add(new Song("Shot for Me", "Drake", "Take Care"));
+        songsArray.add(new Song("Headlines", "Drake", "Take Care"));
+        songsArray.add(new Song("Doing it Wrong", "Drake", "Take Care"));
+        songsArray.add(new Song("Practice", "Drake", "Take Care"));
+        songsArray.add(new Song("Nonstop", "Drake", "Scorpion"));
+        songsArray.add(new Song("God's Plan", "Drake", "Scorpion"));
+        songsArray.add(new Song("I'm Upset", "Drake", "Scorpion"));
+        songsArray.add(new Song("Back to Sleep", "Chris Brown", "Royalty"));
+        songsArray.add(new Song("Deuces", "Chris Brown", "F.A.M.E"));
+        songsArray.add(new Song("H.O.L.Y", "Florida Georgia Line", "Dig Your Roots"));
+        songsArray.add(new Song("Sixteen", "Thomas Rhett", "Life Changes"));
 
-    }
+        TextView nowPlaying = findViewById(R.id.now_playing_text_view);
+        nowPlaying.setText("Song Name:    " + songsArray.get(index).getmSongName() +
+                            "\nArtist Name:    " + songsArray.get(index).getmArtistName() +
+                            "\nAlbum Name:    " + songsArray.get(index).getmAlbumName());
 
-    /*Method to show the playing song when previous button is pressed*/
-    public void previous(View view) {
+        /*OnClick listeners for the fast forward button to display current song*/
+        Button forward = findViewById(R.id.forward_button);
+        forward.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                int i = index;
+                int totalSongs = songsArray.size();
+                if(i < totalSongs){
+                    TextView nowPlaying = findViewById(R.id.now_playing_text_view);
+                    nowPlaying.setText("Song Name:    " + songsArray.get(i).getmSongName() +
+                            "\nArtist Name:    " + songsArray.get(i).getmArtistName() +
+                            "\nAlbum Name:    " + songsArray.get(i).getmAlbumName());
+                    if(i < totalSongs-1){
+                    index++;
+                    }
+                    else{
+                            Toast.makeText(getApplicationContext(), "No more songs in playlist",
+                                    Toast.LENGTH_SHORT).show();
+                    }
+                }
 
+            }
+        });
+
+        Button rewind = findViewById(R.id.rewind_button);
+        rewind.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                int i = index;
+                if(i > -1){
+                    TextView nowPlaying = findViewById(R.id.now_playing_text_view);
+                    nowPlaying.setText("Song Name:    " + songsArray.get(i).getmSongName() +
+                            "\nArtist Name:    " + songsArray.get(i).getmArtistName() +
+                            "\nAlbum Name:    " + songsArray.get(i).getmAlbumName());
+                    if(i>0){
+                    index--;
+                    }
+                    else{
+                        Toast.makeText(getApplicationContext(), "This is the first song in playlist",
+                                Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+        });
     }
 }
